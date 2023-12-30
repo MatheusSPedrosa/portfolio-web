feb497944a9285d2cc327f1f9b4fb3bd32e8d624
Matheus Pedrosa <152664230+MatheusSPedrosa@users.noreply.github.com>
2023-12-29 21:36:19 -0300

    Atualizando profile

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData() {
-    const url = ''
+    const url = 'https://raw.githubusercontent.com/MatheusSPedrosa/portfolio-web/main/data/profile.json'
     const fetching = await fetch(url)
     return await fetching.json()
 }
